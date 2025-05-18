56a6cd2605b023c4fa8003dd4fbe7a4c283ccb37
ham <ham.surel@outlook.com>
2025-05-18 11:00:15 +0700

    Initial Commit

---
+++ SIGHub-Rework/SIGHub_ReworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIGHub_ReworkApp.swift
+//  SIGHub-Rework
+//
+//  Created by Ilham Shahputra on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SIGHub_ReworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
